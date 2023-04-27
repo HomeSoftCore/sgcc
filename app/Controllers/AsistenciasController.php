42dee939a5d2c6040e9bcfc9696dbec25e1e50b6
cbaque <claudio.sys16@gmail.com>
2023-04-27 13:31:27 -0500

    registrar asistencia ok

--- app/Controllers/AsistenciasController.php
+++ app/Controllers/AsistenciasController.php
@@ -60,6 +60,7 @@ class AsistenciasController extends BaseController
 			'content' => 'Asistencias/ListarEstudiantes',
 			'estudiantes' => $estudiantes,
 			'curso' => $curso,
+			'fecha_actual' => date('Y-m-d'),
 		];
 
 		$estructura=	view('Estructura/layout/index', $data);						
@@ -123,102 +124,4 @@ class AsistenciasController extends BaseController
 		return redirect()->to(site_url('/AsistenciasController'));	
 	}
 
-
-
-
-	/*
-    public function nuevo()
-	{
-		$estructura=	view('Estructura/Header').
-						view('Estructura/Menu').
-						view('Areas/AreasNuevo').
-						view('Estructura/Footer');
-
-		return $estructura;
-	}
-
-    public function guardar(){
-		$AreasModel= new AreasModel($db);
-		$request=\Config\Services::request();
-		$data=array(
-			'ARENOMBRE'=>$request->getPostGet('txtAreas'),
-		);
-		//var_dump($data);
-		if($AreasModel->insert($data)===false){
-			var_dump($AreasModel->errors());
-		}
-
-		//redirige a metodo index
-		return redirect()->to(site_url('/AreasController'));	
-	}
-
-	public function editar(){
-		$request=\Config\Services::request();
-		$id = $request->getPostGet('id');
-
-		$AreasModel=new AreasModel($db);
-		$areas=$AreasModel->find($id);
-		$areas=array('areas'=>$areas);
-		//var_dump($areas);
-		$data['areas']=$areas;
-		
-		$estructura=	view('Estructura/Header').
-						view('Estructura/Menu').
-						view('Areas/AreasEditar',$data).
-						view('Estructura/Footer');
-		//$estructura=view('Estructura/Encabezado').view('Areas/AreasEditar',$data).view('Estructura/pie');
-		return $estructura;			
-	}
-
-	public function modificar(){
-		$AreasModel= new AreasModel($db);
-		$request=\Config\Services::request();
-		$data=array(
-			'AREID'=>$request->getPost('txtCodigo'),
-			'ARENOMBRE'=>$request->getPost('txtArea'),
-		);
-		$AREID=$request->getPost('txtCodigo');
-		if($AreasModel->update($AREID,$data)===false){
-			var_dump($AreasModel->errors());
-		}
-
-		//redirige a metodo index
-		return redirect()->to(site_url('/AreasController'));
-	}
-	
-	public function borrar(){
-		$request=\Config\Services::request();
-		$id=$request->getPostGet('id');
-
-		$AreasModel=new AreasModel($db);
-		$areas=$AreasModel->find($id);
-		$areas=array('areas'=>$areas);
-		//var_dump($areas);
-		$data['areas']=$areas;
-		
-		$estructura=	view('Estructura/Header').
-		view('Estructura/Menu').
-		view('Areas/AreasBorrar',$data).
-		view('Estructura/Footer');
-		//$estructura=view('Estructura/Encabezado').view('Areas/AreasBorrar',$data).view('Estructura/pie');
-		return $estructura;		
-	}
-		
-	public function eliminar(){
-		$request=\Config\Services::request();
-		$AreasModel=new AreasModel($db);
-		$id=$request->getPostGet('txtCodigo');
-		$areas=$AreasModel->find($id);
-		$areas=array('areas'=>$areas);
-		
-		if($AreasModel->delete($id)===false){
-			print_r($AreasModel->errors());
-		}else{
-			$AreasModel->purgeDeleted($id);
-		}
-
-		//redirige a metodo index
-		return redirect()->to(site_url('/AreasController'));	
-	}
-*/
 }
--- app/Views/Asistencias/ListarEstudiantes.php
+++ app/Views/Asistencias/ListarEstudiantes.php
@@ -32,7 +32,15 @@
                         <td><?php echo $item['ESTNOMBRE']; ?></td>
                         <td><?php echo $item['ESTCEDULA']; ?></td>
                         <td><?php echo $item['ESTCORREO']; ?></td>
-                        <td><button type="button" onclick="location.href='<?php echo base_url();?>/RegistroCalificacionesController/indexCalificacion?id=<?php echo $item['ESTID'];?>&mat=<?php echo $item['MATID'];?>'" class="btn btn-primary">Marcar Asistencia</button></td>                        
+                        <td>
+                            <button type="button" 
+                                onclick="registrarAsistencia(<?php echo $item['MATID']; ?>, '<?php echo $item['ESTNOMBRE']; ?>')"
+                                class="btn btn-success"
+                                data-toggle="modal"
+                                data-target="#modal-default-asistencia">Marcar Asistencia
+                            </button>
+
+                        </td>                        
                     </tr>
 
                     <?php } ?>
@@ -42,3 +50,69 @@
         </div>
     </div>
 </div>
+
+
+
+<div class="modal fade" id="modal-default-asistencia" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
+    <form action="<?php echo base_url();?>/AsistenciasController/guardar" method="POST" autocomplete="off">
+        <input type="hidden" id="MATID" name="MATID">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="demoModalLabel">Registro de Asistencia <div id="item-data"></div></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            Estudiante: <span id="nombre_estudiante"></span>
+                        </div>                      
+                    </div> 
+                    <br>
+                    <div class="row">
+                        <div class="col-md-12">
+                              <div class="form-group">
+                                <label for="asistencia">Estado</label>
+                                <select name="asistencia" id="asistencia" class="form-control" require>
+                                    <option value="SI">ASISTE</option>
+                                    <option value="NO">NO ASISTE</option>
+                                </select>
+                              </div>
+                        </div>                      
+                    </div>  
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                              <div class="form-group">
+                                  <label for="observacion">Observacion</label>
+                                  <input type="text" class="form-control" id="observacion" name="observacion" value="" placeholder="observacion...">
+                              </div>
+                        </div>                      
+                    </div>  
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                              <div class="form-group">
+                                  <label for="fecha">Fecha</label>
+                                  <input type="date" class="form-control" id="fecha" name="fecha" value="<?php echo $fecha_actual ?>" placeholder="fecha...">
+                              </div>
+                        </div>                      
+                    </div>                     
+                      
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Registrar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<script>
+    function registrarAsistencia(MATID, nombre){
+        $("#modal-default-asistencia #MATID").val(MATID);
+        $("#modal-default-asistencia #nombre_estudiante").html(nombre);
+    }
+</script>
